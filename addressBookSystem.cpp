3be5f87da56f8437936e68d1d170cd6784024441
shivamrajput18 <shivamsingh80094@gmail.com>
2025-01-12 22:21:44 +0530

    Edit contacts

--- addressBookSystem.cpp
+++ addressBookSystem.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class Contact {
@@ -24,16 +25,45 @@ public:
         cout << "Phone Number: " << phoneNumber << endl;
         cout << "Email: " << email << endl;
     }
+
+    string getFullName() const {
+        return firstName + " " + lastName;
+    }
+
+    string getFirstName() const { return firstName; }
+    string getLastName() const { return lastName; }
+
+    void editContact(string addr, string cty, string st, int z, long long phone, string mail) {
+        address = addr;
+        city = cty;
+        state = st;
+        zip = z;
+        phoneNumber = phone;
+        email = mail;
+        cout << "Contact updated successfully.\n";
+    }
 };
+
+string trim(const string& str) {
+    size_t first = str.find_first_not_of(' ');
+    size_t last = str.find_last_not_of(' ');
+    return (first == string::npos || last == string::npos) ? "" : str.substr(first, last - first + 1);
+}
+
+string toLowerCase(const string& str) {
+    string lowerStr = str;
+    transform(lowerStr.begin(), lowerStr.end(), lowerStr.begin(), ::tolower);
+    return lowerStr;
+}
 
 class AddressBook {
 private:
-    vector<Contact> contacts; 
+    vector<Contact> contacts;
 
 public:
     void addContact(string fName, string lName, string addr, string cty, string st, int z, long long phone, string mail) {
-        Contact newContact(fName, lName, addr, cty, st, z, phone, mail); 
-        contacts.push_back(newContact); 
+        Contact newContact(fName, lName, addr, cty, st, z, phone, mail);
+        contacts.push_back(newContact);
         cout << "Contact added successfully.\n";
     }
 
@@ -47,6 +77,52 @@ public:
         for (const auto& contact : contacts) {
             contact.displayContact();
             cout << "-----------------------------\n";
+        }
+    }
+
+    void editContact(string name) {
+        name = trim(name);  
+        name = toLowerCase(name); 
+
+        bool found = false;
+        for (auto& contact : contacts) {
+            string fullName = contact.getFullName();
+            string firstName = contact.getFirstName();
+            string lastName = contact.getLastName();
+
+            string fullNameLower = toLowerCase(fullName);
+            string firstNameLower = toLowerCase(firstName);
+            string lastNameLower = toLowerCase(lastName);
+
+            if (fullNameLower.find(name) != string::npos || firstNameLower.find(name) != string::npos || lastNameLower.find(name) != string::npos) {
+                cout << "Found contact: " << fullName << endl;
+                string addr, cty, st, mail;
+                int z;
+                long long phone;
+
+                cout << "Enter new details for " << fullName << ":\n";
+                cout << "Enter Address: ";
+                cin.ignore(); 
+                getline(cin, addr);
+                cout << "Enter City: ";
+                cin >> cty;
+                cout << "Enter State: ";
+                cin >> st;
+                cout << "Enter ZIP Code: ";
+                cin >> z;
+                cout << "Enter Phone Number: ";
+                cin >> phone;
+                cout << "Enter Email: ";
+                cin >> mail;
+
+                contact.editContact(addr, cty, st, z, phone, mail);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            cout << "Contact with name " << name << " not found.\n";
         }
     }
 };
@@ -84,10 +160,25 @@ int main() {
 
         cout << "Do you want to add another contact? (y/n): ";
         cin >> addMore;
+        cin.ignore(); 
     }
 
     cout << "\nNow displaying all contacts in the Address Book:\n";
     addressBook->displayAllContacts();
+
+    char editMore = 'y';
+    while (editMore == 'y' || editMore == 'Y') {
+        string name;
+        cout << "\nEnter the full name of the contact to edit (First Last), or just First or Last name: ";
+        cin.ignore();  
+        getline(cin, name);
+
+        addressBook->editContact(name); 
+
+        cout << "Do you want to edit another contact? (y/n): ";
+        cin >> editMore;
+        cin.ignore(); 
+    }
 
     delete addressBook;
 
